801c0b0a64b1cd386acf78a203efe1a6638cc9ea
Zhang Lixu <lixu.zhang@intel.com>
2025-01-22 09:29:01 +0800

    HID: intel-ish-hid: Send clock sync message immediately after reset
    [ Upstream commit 7e0d1cff12b895f44f4ddc8cf50311bc1f775201 ]
    
    The ISH driver performs a clock sync with the firmware once at system
    startup and then every 20 seconds. If a firmware reset occurs right
    after a clock sync, the driver would wait 20 seconds before performing
    another clock sync with the firmware. This is particularly problematic
    with the introduction of the "load firmware from host" feature, where
    the driver performs a clock sync with the bootloader and then has to
    wait 20 seconds before syncing with the main firmware.
    
    This patch clears prev_sync immediately upon receiving an IPC reset,
    so that the main firmware and driver will perform a clock sync
    immediately after completing the IPC handshake.
    
    Signed-off-by: Zhang Lixu <lixu.zhang@intel.com>
    Acked-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/hid/intel-ish-hid/ipc/ipc.c
+++ drivers/hid/intel-ish-hid/ipc/ipc.c
@@ -580,13 +580,12 @@ static void fw_reset_work_fn(struct work_struct *unused)
  */
 static void _ish_sync_fw_clock(struct ishtp_device *dev)
 {
-	static unsigned long	prev_sync;
 	struct ipc_time_update_msg time = {};
 
 	if (dev->prev_sync && time_before(jiffies, dev->prev_sync + 20 * HZ))
 		return;
 
-	prev_sync = jiffies;
+	dev->prev_sync = jiffies;
 	/* The fields of time would be updated while sending message */
 	ipc_send_mng_msg(dev, MNG_SYNC_FW_CLOCK, &time, sizeof(time));
 }
